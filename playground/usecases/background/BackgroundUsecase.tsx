84ad06d8ef1721bb47fc9b34d62051eb901d014d
mishamyrt <misha@myrt.co>
2025-09-19 19:18:41 +0300

    style: fix code style

--- playground/usecases/background/BackgroundUsecase.tsx
+++ playground/usecases/background/BackgroundUsecase.tsx
@@ -1,45 +1,45 @@
-import React, { useEffect, useMemo, useState, type FC } from "react";
+import React, { useEffect, useMemo, useState, type FC } from 'react'
 
-import { createCheatsListener } from "../../../src/index.js";
-import clsx from "clsx";
-import type { Usecase } from "../types.js";
-import styles from "./BackgroundUsecase.module.css";
+import { createCheatsListener } from '../../../src/index.js'
+import clsx from 'clsx'
+import type { Usecase } from '../types.js'
+import styles from './BackgroundUsecase.module.css'
 
 const cheatsMap: Record<number, string> = {
-  0x0700e608: "hesoyam",
-  0x0800dc07: "aspirine",
-  0x0b06540f: "fannymagnet",
-  0x0f01420f: "fightfightfight",
-  0x0a00da08: "alovelyday",
-  0x1100730b: "professionaltools",
-  0x06000008: "panzer",
-};
+  0x0700e608: 'hesoyam',
+  0x0800dc07: 'aspirine',
+  0x0b06540f: 'fannymagnet',
+  0x0f01420f: 'fightfightfight',
+  0x0a00da08: 'alovelyday',
+  0x1100730b: 'professionaltools',
+  0x06000008: 'panzer'
+}
 
 const BackgroundUsecaseComponent: FC = () => {
-  const [activated, setActivated] = useState<string[]>([]);
+  const [activated, setActivated] = useState<string[]>([])
 
-  const cheatNames = useMemo(() => Object.values(cheatsMap), []);
+  const cheatNames = useMemo(() => Object.values(cheatsMap), [])
   const cheats: number[] = useMemo(
     () => Object.keys(cheatsMap).map((key) => parseInt(key)),
     []
-  );
+  )
 
   useEffect(() => {
     const { start, stop } = createCheatsListener({
       onCheat: (cheat: number) => {
-        const name = cheatsMap[cheat];
+        const name = cheatsMap[cheat]
         setActivated((prev) => {
           if (prev.includes(name)) {
-            return prev.filter((n) => n !== name);
+            return prev.filter((n) => n !== name)
           }
-          return [...prev, name];
-        });
+          return [...prev, name]
+        })
       },
-      cheats,
-    });
-    start();
-    return stop;
-  }, []);
+      cheats
+    })
+    start()
+    return stop
+  }, [])
 
   return (
     <div>
@@ -53,7 +53,7 @@ const BackgroundUsecaseComponent: FC = () => {
           <li
             key={name}
             className={clsx(styles.cheat, {
-              [styles.activated]: activated.includes(name),
+              [styles.activated]: activated.includes(name)
             })}
           >
             {name}
@@ -61,11 +61,11 @@ const BackgroundUsecaseComponent: FC = () => {
         ))}
       </ul>
     </div>
-  );
-};
+  )
+}
 
 export const BackgroundUsecase: Usecase = {
-  name: "Background",
-  directory: "background",
-  Component: BackgroundUsecaseComponent,
-};
+  name: 'Background',
+  directory: 'background',
+  Component: BackgroundUsecaseComponent
+}
